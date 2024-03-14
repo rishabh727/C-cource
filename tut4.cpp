f2107cddefe00d67e5bdca732ecd466e77969bb4
rishabh727 <143151167+rishabh727@users.noreply.github.com>
2024-03-14 19:50:45 +0530

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=5;
+    int b=7;
+    float pi= 3.14;
+    char c= 'v';
+    cout<<"value of a is  "<<a<<  "\n value of b is"<<b;
+    cout<<"\nvalue of pi is "<<pi;
+    cout<<"\nthe value of c is  "<<c;
+    return 0;
+}
