ae1ca878e23efdef24af8a09e91787141fcab213
rishabh727 <143151167+rishabh727@users.noreply.github.com>
2024-03-14 19:56:06 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2 ;
+    cout<<" enter the value of num 1: \n";/*this is a Insertion oprater*/
+    cin>>num1;/*this is called exterction oprator*/
+
+
+    cout<<" enter the value of num 2: \n";/*this is a Insertion oprater*/
+    cin>>num2;/*this is called exterction oprator*/
+    cout<<"the sum is: "<< num1+num2;
+}
